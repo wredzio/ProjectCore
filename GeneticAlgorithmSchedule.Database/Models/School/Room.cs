973eee8d3bd5aca6fbda34ecac94d637bb58cb1b
wojciech.szmidt <Wojciech.Szmidt@onwelo.com>
2017-09-27 11:12:22 +0200

    auth2

--- GeneticAlgorithmSchedule.Database/Models/School/Room.cs
+++ GeneticAlgorithmSchedule.Database/Models/School/Room.cs
@@ -1,4 +1,5 @@
 ﻿
+using GeneticAlgorithmSchedule.Database.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- GeneticAlgorithmSchedule.Database/Models/School/StudentGroupCourseClass.cs
+++ GeneticAlgorithmSchedule.Database/Models/School/StudentGroupCourseClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneticAlgorithmSchedule.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
