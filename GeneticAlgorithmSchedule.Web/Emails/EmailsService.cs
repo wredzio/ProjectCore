9984301179178b735099dd60370ebb1b3183f950
Woj <wojciech.szmidt@gmail.com>
2018-02-07 18:39:43 +0100

    Dodanie reszty

---
+++ GeneticAlgorithmSchedule.Web/Emails/EmailsService.cs
@@ -0,0 +1,19 @@
+﻿using GeneticAlgorithmSchedule.Web.Emails.EmailBuilders;
+using GeneticAlgorithmSchedule.Web.Emails.PostBoxs;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithmSchedule.Web.Emails
+{
+    public static class EmailsService
+    {
+        public static void AddEmailsService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IEmailBuilderFactory, EmailBuilderFactory>();
+            serviceCollection.AddSingleton<IPostBox, PostBox>();
+        }
+    }
+}
